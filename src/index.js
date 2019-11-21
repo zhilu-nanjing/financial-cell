cf14cc880999430a47e66d74e8744a9075d7127b
wen651788064 <651788064@qq.com>
2019-11-21 10:23:17 +0800

    update

--- src/index.js
+++ src/index.js
@@ -21,11 +21,7 @@ class Spreadsheet {
             .on('contextmenu', evt => evt.preventDefault());
         // create canvas element
         targetEl.appendChild(rootEl.el);
-        // const worker = new Worker();
-        // worker.postMessage({ a: 1 });
-        // worker.onmessage = function (event) {};
-        //
-        // worker.addEventListener("message", function (event) {});
+
         this.sheet = new Sheet(rootEl, this.data);
         this.data.sheet = this.sheet;
         this.plugIn = new PlugIn(rootEl, this.sheet, this.data);
