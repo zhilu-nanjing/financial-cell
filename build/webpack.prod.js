0edeae1c06e54e03a55054816c6508769a145299
wen651788064 <651788064@qq.com>
2019-11-26 09:48:39 +0800

    update

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -4,11 +4,13 @@ const common = require('./webpack.config.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
     mode: 'production',
     devtool: 'source-map',
     plugins: [
+        new BundleAnalyzerPlugin(),
         new CleanWebpackPlugin(['dist']),
         //  you should know that the HtmlWebpackPlugin by default will generate its own index.html
         new HtmlWebpackPlugin({
